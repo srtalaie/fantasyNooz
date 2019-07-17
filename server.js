33cb0b8eec20445253bc798200f17ff5aaabc589
srtalaie <srt4721@yahoo.com>
2019-07-17 15:48:34 -0700

    Trying to scrape from ADP site

--- server.js
+++ server.js
@@ -3,10 +3,21 @@ require("dotenv").config();
 const express = require('express');
 const path = require("path");
 const bodyParser = require("body-parser");
+const cheerio = require("cheerio");
+const request = require("request");
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
+
+app.get('/getADP', function(req, res){
+  request('https://www.4for4.com/fantasy-football/adp', function(error, response, html){
+    let $ = cheerio.load(html);
+
+    table = $('table').html();
+    console.log(table);
+  });
+});
 
 // Define middleware here
 app.use(bodyParser.urlencoded({ extended: true }));
