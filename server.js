83268452a776d739ed5c4bceef20c2d2571fd76f
srtalaie <srt4721@yahoo.com>
2019-08-19 15:41:53 -0700

    Deploy to heroku

--- server.js
+++ server.js
@@ -11,9 +11,9 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 
 // Define middleware here
-app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
