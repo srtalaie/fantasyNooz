c0676e7dd52882d04df6678746fbde1b42c3ff56
srtalaie <srt4721@yahoo.com>
2019-08-19 21:44:26 -0700

    built out basic navbar on homepage

--- client/src/App.js
+++ client/src/App.js
@@ -3,14 +3,36 @@ import ADPTable from './pages/ADPTable/ADPTable.js';
 import Articles from './pages/Articles/Articles.js';
 import Stats from './pages/Stats/Stats.js'
 import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+import './style.css';
 
 function App() {
   return (
-    <div>
-      <header>
-      <Stats />
-      </header>
-    </div>
+    <Router>
+      <div>
+        <div>
+          <ul className="navbar">
+            {/* <li>
+              <Link to="/">Home</Link>
+            </li> */}
+            <li className="navStyle">
+              <Link to="/adptable/">ADP Table</Link>
+            </li>
+            <li className="navStyle">
+              <Link to="/articles/">Articles</Link>
+            </li>
+            <li className="navStyle">
+              <Link to="/stats/">Stats</Link>
+            </li>
+          </ul>
+        </div>
+        {/* <Route path="/" exact component={Index} />  NO INDEX YET*/}
+        <Switch>
+          <Route path="/adptable/" component={ADPTable} />
+          <Route path="/articles/" component={Articles} />
+          <Route path="/stats/" component={Stats} />
+        </Switch> 
+      </div>
+    </Router>   
   );
 }
 
